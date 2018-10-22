e32d714b31dd9f31115c970a053a5c48f3c71460
Anobium <evan@anobium.co.uk>
2018-10-22 23:35:33 +0100

    Updated to handle .git

--- source/dailybuild.bat
+++ source/dailybuild.bat
@@ -2,7 +2,7 @@ del D:\Great-Cow-BASIC-Help.git\trunk\output\chm\gcbasic.chm
 svn up d:\great*
 call all
 :jump1
-if exist D:\Great-Cow-BASIC-Help\trunk\output\chm\gcbasic.chm ( 
+if exist D:\Great-Cow-BASIC-Help.git\trunk\output\chm\gcbasic.chm ( 
 "C:\Program Files\FileZilla FTP Client\psftp.exe"  -v evanvennn@web.sourceforge.net -pw password123 -b BatchFile.psftp
 blat.exe -serverSMTP smtp.123-reg.co.uk:25 -u evan@anobium.co.uk -pw Anobium1! -f evan@anobium.co.uk -to evan@anobium.co.uk -subject "Daily build OK for Help" -body "Completed"
 ) else (
